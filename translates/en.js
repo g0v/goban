ec63274bc34bfbf8773135402231fa091871217e
Bestian Tang <bestian@gmail.com>
2015-01-02 16:27:17 +0800

    en.js

--- translates/en.js
+++ translates/en.js
@@ -3,8 +3,8 @@ angular.module("automap")
       TITLE: 'g0v blackboard',
       LONG_TITLE: 'hacker\'s goban',
       OPEN_BLANK: 'Click here to open a new tab',
-      EDIT: 'edit',
-      CREATE: 'create',
+      EDIT: 'edit ',
+      CREATE: 'create ',
       'TO CREATE A GOBAN, JAST VISIT https://goban.tw/YOUR_FOLDER' : 'to create a goban, just visit https://goban.tw/YOUR_FOLDER',
       WEB_CONFIG: 'configurate via web'
     })
