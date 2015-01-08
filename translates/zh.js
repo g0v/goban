7fa23839ddf4090b5789eedfab31359d39d911ba
Bestian Tang <bestian@gmail.com>
2015-01-08 12:14:09 +0800

    titile hint

--- translates/zh.js
+++ translates/zh.js
@@ -1,27 +1,54 @@
 angular.module("automap")
 .constant("$zhTW", {
+    BACKUP: '備份下載',
+    LEVELUP : '前推',
+    LEVELDOWN : '倒溯',
+    //
+    UP: '向前',
+    DOWN: '向後',
+    SACLE: '縮放',
+    //
   	TITLE: '零時黑板',
     LONG_TITLE: '黑客棋盤',
   	OPEN_BLANK: '按此可開啟新頁籤',
-    MY_STAR: '我的珍藏',
-    GATHER: '採集',
+    //
+    MY_STAR: '還記得',
+    GATHER: '加入/移除',
+    ZAZAN: '全放下/全回復',
+    //
     EDIT: '編輯',
   	CREATE: '創建',
+    //
+    'LAST TIP':'上一則提示',
+    'NEXT TIP':'下一則提示',
+      WEB_CONFIG: '聯想設定',
+    //
   	'TO CREATE A GOBAN, JAST VISIT https://goban.tw/YOUR_FOLDER' : '前往"https://goban.tw/您的檔名"，即可創建您的黑客棋盤',
   	'CLICK "CREATE" TO ENTER EDITOR' : '點擊「創建」進入編輯器',
 	'IN ETHERCALC EDITOR, TYPE YOUR_TITLE INTO B2 CELL': '在「醚人算表」編輯介面中的B2儲存格內，鍵入您的標題',
 	'PASTE AN URL YOU LIKE INTO A3 CELL': '貼一則您喜歡的網址上A3儲存格中',
 	'TYPE A NAME OF THIS URL INTO B3 CELL': '為它選一個標題，打上B3儲存格中',
 	'RELOAD TO ENJOY YOUR FIRST GOBAN': '重新載入即可享受您的第一個黑客棋盤',
-	'LAST TIP':'上一則提示',
-	'NEXT TIP':'下一則提示',
-    WEB_CONFIG: '網上設定'	
+
     })
 .constant("$zhCN", {
+      BACKUP: '备份下载',
+      LEVELUP : '前推',
+      LEVELDOWN : '倒溯',
+      //
+      UP: '向前',
+      DOWN: '向后',
+      SACLE: '缩放',
+      //
       TITLE : '零時黑板',
       LONG_TITLE: '黑客棋盘',
       OPEN_BLANK: '按此可开启新页签',
+      //
+      MY_STAR: '还记得',
+      GATHER: '加入/移除',
+      ZAZAN: '全放下/全回复',
+      //
       EDIT: '编辑',
       CREATE: '创建',
-      WEB_CONFIG: '网上设定'  
+      WEB_CONFIG: '联想设定',
     })
