704bffe904030a6b9209a2f617f439f30d8d0d47
Bestian Tang <bestian@gmail.com>
2020-02-17 19:52:12 +0800

    use autofocus

--- old/translates/ja.js
+++
@@ -1,8 +0,0 @@
-angular.module("automap")
-.constant("$ja", {
-      TITLE: '黒板',
-      LONG_TITLE: 'ハッカーのチェス盤',
-      OPEN_BLANK: '新しいタブを開くには、ここをクリックしてください',
-      EDIT: 'エディタ',
-      CREATE: '作る'
-    })
