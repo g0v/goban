4f296760768e73b5e5d26f1142d4c3d1179c9313
Bestian Tang <bestian@gmail.com>
2020-03-19 10:09:26 +0800

    Update stars.js
    debug

--- src/mixins/stars.js
+++ src/mixins/stars.js
@@ -3,6 +3,7 @@
 export default {
   methods: {
      checkJSON: function (text) {
+      if (!text) { return false}
       if (/^[\],:{}\s]*$/.test(text.replace(/\\["\\\/bfnrtu]/g, '@').replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').replace(/(?:^|:|,)(?:\s*\[)+/g, ''))) {
         return true
       } else {
