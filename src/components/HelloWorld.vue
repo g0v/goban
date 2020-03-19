ded934c25e2243f579e3da375e00656eb3f13f29
Bestian Tang <bestian@gmail.com>
2020-03-19 17:09:27 +0800

    build
    debug app link

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,14 +2,6 @@
   .hello
     h1.ui.header {{ $t('hello') }}
     h3.sub.header {{ $t('hello2') }}
-    .ui.button.group.thin-only()
-      a.ui.orange.button(v-show="!iOS()", href="https://play.google.com/store/apps/details?id=tw.goban.app", target="_blank")
-        i.google.play.icon
-        | App on Google Play
-      a.ui.green.button.thin-only(v-show="iOS()", href="https://apps.apple.com/us/app/%E9%9B%B6%E6%99%82%E9%BB%91%E6%9D%BF/id1500095725?l=zh&ls=1", target="_blank")
-        i.app.store.icon
-        | App on App Store
-      | &nbsp;
     .ui.form.container
       .field
         input(v-autofocus="", type='search', name='', v-model='myKey', placeholder='搜尋或創建新黑板', autofocus='true', list="gs" @keydown.enter = "($router.push('/see/' + myKey + '/0/0'))")
--- src/components/MyApp.vue
+++ src/components/MyApp.vue
@@ -3,10 +3,10 @@
     vue-headful(title="手機App@零時黑板", description="手機App@零時黑板")
     h1.ui.header 零時黑板-手機App
     .ui.button.group
-      a.ui.orange.huge.button(v-show="!iOS()" href="https://play.google.com/store/apps/details?id=tw.goban.app", target="_blank")
+      a.ui.orange.huge.button(href="https://play.google.com/store/apps/details?id=tw.goban.app", target="_blank")
         i.google.play.icon
         | App on Google Play
-      a.ui.green.button(v-show="iOS()", href="https://apps.apple.com/us/app/%E9%9B%B6%E6%99%82%E9%BB%91%E6%9D%BF/id1500095725?l=zh&ls=1", target="_blank")
+      a.ui.green.huge.button(href="https://apps.apple.com/us/app/%E9%9B%B6%E6%99%82%E9%BB%91%E6%9D%BF/id1500095725?l=zh&ls=1", target="_blank")
         i.app.store.icon
         | App on App Store
 
@@ -19,25 +19,6 @@ export default {
     }
   },
   methods: {
-    iOS: function () {
-      var ans = false
-      var iDevices = [
-        'iPad Simulator',
-        'iPhone Simulator',
-        'iPod Simulator',
-        'iPad',
-        'iPhone',
-        'iPod'
-      ]
-      // console.log(navigator.platform)
-      if (navigator.platform) {
-        while (iDevices.length) {
-          if (navigator.platform === iDevices.pop()) { ans = true }
-        }
-      }
-      // console.log(ans)
-      return ans
-    }
   }
 }
 </script>
