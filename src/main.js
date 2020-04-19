8e9568225fd947b6faac972ff44e0bd7cdd2288f
Bestian Tang <bestian@gmail.com>
2020-04-20 07:05:23 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import { rtdbPlugin } from 'vuefire'
-import axios from 'axios';
-import VueAxios from 'vue-axios';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 import SuiVue from 'semantic-ui-vue'
 import 'semantic-ui-css/semantic.min.css'
 import VueLocalStorage from 'vue-localstorage'
